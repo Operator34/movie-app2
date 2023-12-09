bcc44e1db878e954e439ac5b1532dae296d8250e
Andrey Loginov <operator34@yandex.ru>
2023-12-09 18:13:42 +0300

    test husky

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-    return <div className="App">Hello Movie</div>;
+  return <div className="App">Hello Movie</div>;
 }
 
 export default App;
